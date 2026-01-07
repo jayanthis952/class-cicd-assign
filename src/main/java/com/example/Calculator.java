2a1287449ee8b89e48ca691d9d73c0c68ead0a05
Agasthyahm <hmagasthya@gmail.com>
2026-01-07 23:55:08 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,6 +4,7 @@ public class Calculator {
 
     public static void main(String[] args) {
         System.out.println("Calculator app started");
+        Thread.sleep(Long.MAX_VALUE);
     }
 
     public int add(int a, int b) {
